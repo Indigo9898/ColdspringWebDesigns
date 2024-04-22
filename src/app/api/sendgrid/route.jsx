8b73033ee1f6ed41bb800e6d31601c0bd38396d3
ausdyer <adyer1717@gmail.com>
2024-04-22 10:08:34 -0500

    promise added to mailer

--- src/app/api/sendgrid/route.jsx
+++ src/app/api/sendgrid/route.jsx
@@ -1,26 +1,38 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
-export async function POST(req, res){
-    const body = await req.json()
-    
+export async function POST(req, res) {
+  const body = await req.json();
+
+  const nodemailer = require("nodemailer");
+
+  let mailTransporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "adyer1717@gmail.com",
+      pass: "zpxhahtzggtsudvl",
+    },
+  });
+
+  let mailDetails = {
+    from: "adyer1717@gmail.com",
+    to: "adyer1717@gmail.com",
+    subject: "Portfolio Inquiry",
+    text:
+      body.email +
+      ", " +
+      body.phone +
+      ", " +
+      body.name +
+      ", " +
+      body.business +
+      ", " +
+      body.message,
+  };
+
   
-    const nodemailer = require("nodemailer");
-  
-    let mailTransporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "adyer1717@gmail.com",
-        pass: "zpxhahtzggtsudvl",
-      },
-    });
-  
-    let mailDetails = {
-      from: "adyer1717@gmail.com",
-      to: "adyer1717@gmail.com",
-      subject: "Portfolio Inquiry",
-      text: body.email + ", " + body.phone + ", " + body.name + ", " + body.business + ", " + body.message
-    }; 
-  
+
+  await new Promise((resolve, reject) => {
+    // send mail
     mailTransporter.sendMail(mailDetails, function (err, data) {
       if (err) {
         console.log("Error Occurs");
@@ -28,7 +40,7 @@ export async function POST(req, res){
         console.log("Email sent successfully");
       }
     });
+  });
 
-    return NextResponse.json({ body }, {status: 200})
-  }
-  
+  return NextResponse.json({ body }, { status: 200 });
+}

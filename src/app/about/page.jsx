ead38900f62e16d2b54b1705bec87bc64a12f7e3
ausdyer <adyer1717@gmail.com>
2024-07-15 13:46:31 -0500

    test

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,11 +9,12 @@ import Footer from "../Components/Footer";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen  flex-col bg-white items-center justify-between">
-      <div className="min-h-screen w-full   flex flex-col items-center">
+    <main className="flex min-h-screen  flex-col bg-white items-center justify-between overflow-x-hidden">
+      <div className="min-h-90screen w-full   flex flex-col items-center">
         {/* Header */}
-        <InterorPageHeading activePage="about" pageName="About Us" />
-        <div className="min-h-screen mt-16 flex flex-col items-center p-8 md:pr-32 md:pl-32  2xl:pr-96 2xl:pl-96">
+        <InterorPageHeading activePage="services" pageName="Services" />
+        <div className="min-h-screen flex flex-col items-center text-center bg-white">
+          {/* Your main content */}
           <AboutMeSection />
           <QualityOverQuanitySection />
           <CirclesSection />
